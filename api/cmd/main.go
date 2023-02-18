2c4339ebb5983ab3fb668e83c94c4050d26efc6b
Manojna <manojna.gopher@gmail.com>
2023-02-18 23:00:00 +0530

    feat: setup echo app

---
+++ api/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello world")
+	})
+	if err := e.Start(":5005"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
